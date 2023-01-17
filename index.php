3e2ec5773399444bd2883499c1815452b391ff5d
David Godard <davidgodard76700@gmail.com>
2023-01-17 14:38:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="assets/img/calendar.png">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap-icons-1.9.1/bootstrap-icons.css">
+  <link rel="stylesheet" href="assets/style.css">
+  <title>Calendrier</title>
+</head>
+
+<body>
+  <h1 class=" text-center my-5">Connexion</h1>
+  <div class="container">
+    <div class="row">
+      <form action="" method="POST" class="col-lg-6 mx-auto">
+
+        <div class="mb-3">
+          <label for="email" class="form-label fw-bold">Identifiant</label>
+          <input type="email" class="form-control" id="email" name="mail">
+        </div>
+
+        <div class="mb-3">
+          <label for="password" class="form-label fw-bold">Mot de passe</label>
+          <input type="password" class="form-control" id="password" name="password">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Valider</button>
+      </form>
+    </div>
+    <div class="row my-5">
+      <div class="col-lg-6 mx-auto">
+        <button type="button" class="btn btn-success"><a class="text-decoration-none text-white" href="/inscription.php">Créer un nouveau compte</a></button>
+      </div>
+    </div>
+
+
+  </div>
+
+
+  <script src="assets/js/bootstrap.bundle.js"></script>
+  <script src="script.js"></script>
+</body>
+
+</html>
---
+++ inscription.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/img/calendar.png">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap-icons-1.9.1/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Calendrier</title>
+</head>
+<body>
+  <i class="bi bi-arrow-left-circle-fill fs-1 mx-3" ></i>
+<h1 class=" text-center">Inscription</h1>
+    <div class="container">
+        
+        <form action="" method="POST" class="col-lg-6 mx-auto">
+          <div class="mb-3">
+            <label for="firstname" class="form-label fw-bold">Prénom</label>
+            <input type="text" class="form-control" id="firstname" name="firstname">
+          </div>
+          <div class="mb-3">
+            <label for="name" class="form-label fw-bold">Nom</label>
+            <input type="text" class="form-control" id="name" name="name">
+          </div>
+            <div class="mb-3">
+              <label for="email" class="form-label fw-bold">Adresse email</label>
+              <input type="email" class="form-control" id="email" name="mail">
+            </div>
+            <div class="mb-3">
+                <label for="confirmMail" class="form-label fw-bold">Confirmer votre email</label>
+                <input type="email" class="form-control" id="confirmMail"  name="confirMmail">
+              </div>
+            <div class="mb-3">
+              <label for="password" class="form-label fw-bold">Veuillez choisir un mot de passe</label>
+              <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="mb-3">
+                <label for="confirmPass" class="form-label fw-bold">Confirmer le mot de passe</label>
+                <input type="password" class="form-control" id="confirmPass" name="confirmPass">
+              </div>
+            <div class="mb-3 form-check">
+              <input type="checkbox" class="form-check-input" id="CGU" name="CGU">
+              <label class="form-check-label fw-bold" for="CGU">Veuillez accepter les CGU</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Valider</button>
+    </form>
+    </div>
+
+
+
+
+    <script src="assets/js/bootstrap.bundle.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
