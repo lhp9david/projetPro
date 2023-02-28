e10fa6d8a1243a4ba19f7b7e9c55152175cc5fd3
David Godard <davidgodard76700@gmail.com>
2023-02-28 15:35:56 +0100

    config BDD

--- config/env.php
+++ config/env.php
@@ -1,5 +1,5 @@
 <?php
 
-define('DSN', 'mysql:host=localhost;dbname=project;charset=utf8');
-define('USER', 'root');
-define('PASSWORD', '');
+define('DSN', 'mysql:host=localhost;dbname=clinique;charset=utf8');
+define('USER', 'clinique');
+define('PASSWORD', 'clinique');
