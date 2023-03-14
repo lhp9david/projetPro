16bd257181c4df8e34d07d2b8a2add57f458ffdb
David Godard <davidgodard76700@gmail.com>
2023-03-14 16:13:20 +0100

    aj

--- views/include/navbar.php
+++ views/include/navbar.php
@@ -1,13 +1,13 @@
 
 <nav class="navbar navbar-expand-lg bg-gold">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="/assets/img/calendar.png" alt="logo"></a>
+    <a class="navbar-brand" href="../controllers/controller-accueil.php?month=<?=date('m')?>&year=<?=date('Y')?>"><img src="/assets/img/calendar.png" alt="logo"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="../controllers/controller-accueil.php">Accueil</a>
+        <a class="nav-link active" aria-current="page" href="../controllers/controller-accueil.php?month=<?=date('m')?>&year=<?=date('Y')?>">Accueil</a>
         <a class="nav-link" href="../controllers/controller-documents.php">Documents</a>
         <a class="nav-link" href="../controllers/controller-rdv.php">Evenements</a>
         <a class="nav-link" href="../controllers/controller-login.php?logout">Deconnexion</a>
